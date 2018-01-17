23a1e611b50c20f9f833057b541245254ce5cc3f
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-17 14:41:42 -0600

    Update: README.md documents notebooks

---
+++ notebooks/Users/rymurph@microsoft.com/NYCTaxi/01-MountBlobStorage.scala
@@ -0,0 +1,19 @@
+// Databricks notebook source
+// Mount Azure Blob Storage (supported in Databricks runtime 4 and >)
+// Source dataset
+dbutils.fs.mount(
+  source = "wasbs://nyc@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/source",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
+// Mount Azure Blob Storage (supported in Databricks runtime 4 and >)
+// Raw dataset
+dbutils.fs.mount(
+  source = "wasbs://raw@gaialabsa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/raw",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
