ee665a70ac49997ea2a221da47baedb45ef293a2
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-17 14:50:13 -0600

    Update: README.md documents notebooks

---
+++ notebooks/Users/rymurph@microsoft.com/NYCTaxi/03-LoadGreenTaxiData.scala
@@ -0,0 +1,7 @@
+// Databricks notebook source
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType,LongType,FloatType,DoubleType, TimestampType}
+import com.databricks.backend.daemon.dbutils.FileInfo
+import org.apache.spark.sql.functions._
+import org.apache.hadoop.fs.{ FileSystem, Path }
+import org.apache.hadoop.conf.Configuration
