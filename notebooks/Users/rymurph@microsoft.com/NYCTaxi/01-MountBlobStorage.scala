4f4a4372fb6458c83130422fc779e8d4e7e7742b
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-17 17:42:24 -0600

    Update: README.md documents notebooks

--- notebooks/Users/rymurph@microsoft.com/NYCTaxi/01-MountBlobStorage.scala
+++ notebooks/Users/rymurph@microsoft.com/NYCTaxi/01-MountBlobStorage.scala
@@ -3,7 +3,7 @@
 // Source dataset
 dbutils.fs.mount(
   source = "wasbs://nyc@gaiasa.blob.core.windows.net/",
-  mountPoint = "/mnt/data/nyctaxi/source",
+  mountPoint = "/mnt/data/nyctaxi/source/",
   extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
 
 // COMMAND ----------
@@ -11,9 +11,14 @@ dbutils.fs.mount(
 // Mount Azure Blob Storage (supported in Databricks runtime 4 and >)
 // Raw dataset
 dbutils.fs.mount(
-  source = "wasbs://raw@gaialabsa.blob.core.windows.net/",
-  mountPoint = "/mnt/data/nyctaxi/raw",
+  source = "wasbs://raw@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/raw/",
   extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
+//Refresh mounts
+dbutils.fs.refreshMounts()
 
 // COMMAND ----------
 
