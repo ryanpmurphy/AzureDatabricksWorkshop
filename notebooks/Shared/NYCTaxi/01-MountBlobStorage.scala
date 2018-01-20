13287dc4e8a4a5881fb3aa348373fc05ca01ad6d
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-20 14:52:04 -0600

    Update: README.md documents notebooks

--- notebooks/Shared/NYCTaxi/01-MountBlobStorage.scala
+++ notebooks/Shared/NYCTaxi/01-MountBlobStorage.scala
@@ -21,5 +21,37 @@ dbutils.fs.mount(
 
 // COMMAND ----------
 
+//Mount for consumable reference data
+dbutils.fs.mount(
+  source = "wasbs://refdata@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/refdata/",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
+//Mount for transfomed taxi data
+dbutils.fs.mount(
+  source = "wasbs://transformed@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/transformed/",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
+//Mount for materialized views
+dbutils.fs.mount(
+  source = "wasbs://materialized-views@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/materialized-views/",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
+//Mount for reports
+dbutils.fs.mount(
+  source = "wasbs://reports@gaiasa.blob.core.windows.net/",
+  mountPoint = "/mnt/data/nyctaxi/reports/",
+  extraConfigs = Map("fs.azure.account.key.gaiasa.blob.core.windows.net" -> "bST3bnOVxbdEdKuevIp2JlAjUc4mu81CAxBcmU+w7JfGzQ2SZG8B4sQwnxrFnRoH18KzYlDAvgdogK7FN0wpYQ=="))
+
+// COMMAND ----------
+
 //Refresh mounts
 dbutils.fs.refreshMounts()
