1d6e1be41609fcaee8e8fb2eb309229989b292ce
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-20 11:49:46 -0600

    Update: README.md documents notebooks

--- notebooks/Shared/NYCTaxi/00-CreateDatabaseObjects.scala
+++ notebooks/Shared/NYCTaxi/00-CreateDatabaseObjects.scala
@@ -44,6 +44,44 @@
 // MAGIC %sql
 // MAGIC use nyc_db;
 // MAGIC 
+// MAGIC DROP TABLE IF EXISTS trips_green_raw_prq;
+// MAGIC 
+// MAGIC CREATE TABLE IF NOT EXISTS trips_green_raw_prq(
+// MAGIC taxi_type STRING,
+// MAGIC vendor_id STRING,
+// MAGIC pickup_datetime TIMESTAMP,
+// MAGIC dropoff_datetime TIMESTAMP,
+// MAGIC store_and_fwd_flag STRING,
+// MAGIC rate_code_id INT,
+// MAGIC pickup_location_id INT,
+// MAGIC dropoff_location_id INT,
+// MAGIC pickup_longitude STRING,
+// MAGIC pickup_latitude STRING,
+// MAGIC dropoff_longitude STRING,
+// MAGIC dropoff_latitude STRING,
+// MAGIC passenger_count INT,
+// MAGIC trip_distance DOUBLE,
+// MAGIC fare_amount DOUBLE,
+// MAGIC extra DOUBLE,
+// MAGIC mta_tax DOUBLE,
+// MAGIC tip_amount DOUBLE,
+// MAGIC tolls_amount DOUBLE,
+// MAGIC ehail_fee DOUBLE,
+// MAGIC improvement_surcharge DOUBLE,
+// MAGIC total_amount DOUBLE,
+// MAGIC payment_type INT,
+// MAGIC trip_type INT,
+// MAGIC trip_year STRING,
+// MAGIC trip_month STRING)
+// MAGIC USING parquet
+// MAGIC partitioned by (trip_year,trip_month)
+// MAGIC LOCATION 'wasbs://raw@gaiasa.blob.core.windows.net/green-taxi/';
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC use nyc_db;
+// MAGIC 
 // MAGIC --msck repair table trips_yellow_raw_prq
 // MAGIC --show partitions trips_yellow_raw_prq 
 // MAGIC --alter table trips_yellow_raw_prq add if not exists partition (trip_year=2016,trip_month=01) LOCATION 'wasbs://raw@gaiasa.blob.core.windows.net/yellow-taxi/trip_year=2016/trip_month=01';
