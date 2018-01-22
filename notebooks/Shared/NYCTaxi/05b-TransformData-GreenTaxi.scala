5c1ba859c57180e0459e21894cd70af040e53ddf
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-22 00:43:40 -0600

    Update: README.md documents notebooks

--- notebooks/Shared/NYCTaxi/05b-TransformData-GreenTaxi.scala
+++ notebooks/Shared/NYCTaxi/05b-TransformData-GreenTaxi.scala
@@ -131,9 +131,6 @@ allFiles(destDataDirRoot).foreach((path) => {
 
 //Add partitions to transformed table
 
-import spark.implicits._
-import spark.sql
-
 for (j <- 2014 to 2017)
   {
     val monthsCount = if (j==2017) 6 else 12 
