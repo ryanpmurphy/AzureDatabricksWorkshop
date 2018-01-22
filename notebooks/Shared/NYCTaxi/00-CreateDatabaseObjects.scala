c7419f173f2554dc9dec9d5b394e8ff4bbf1f077
ryanpmurphy <35276726+ryanpmurphy@users.noreply.github.com>
2018-01-22 00:11:45 -0600

    data type updates

--- notebooks/Shared/NYCTaxi/00-CreateDatabaseObjects.scala
+++ notebooks/Shared/NYCTaxi/00-CreateDatabaseObjects.scala
@@ -31,7 +31,7 @@
 // MAGIC tolls_amount DOUBLE,
 // MAGIC improvement_surcharge DOUBLE,
 // MAGIC total_amount DOUBLE,
-// MAGIC payment_type INT,
+// MAGIC payment_type STRING,
 // MAGIC trip_year STRING,
 // MAGIC trip_month STRING)
 // MAGIC USING parquet
@@ -49,7 +49,7 @@
 // MAGIC 
 // MAGIC CREATE TABLE IF NOT EXISTS trips_green_raw_prq(
 // MAGIC taxi_type STRING,
-// MAGIC vendor_id STRING,
+// MAGIC vendor_id INT,
 // MAGIC pickup_datetime TIMESTAMP,
 // MAGIC dropoff_datetime TIMESTAMP,
 // MAGIC store_and_fwd_flag STRING,
@@ -115,7 +115,7 @@
 // MAGIC DROP TABLE IF EXISTS refdata_rate_code_lookup;
 // MAGIC 
 // MAGIC CREATE TABLE IF NOT EXISTS refdata_rate_code_lookup(
-// MAGIC ratecodeid INT,
+// MAGIC rate_code_id INT,
 // MAGIC description STRING)
 // MAGIC USING parquet
 // MAGIC LOCATION 'wasbs://refdata@gaiasa.blob.core.windows.net/rate-code/';
@@ -129,6 +129,7 @@
 // MAGIC 
 // MAGIC CREATE TABLE IF NOT EXISTS refdata_payment_type_lookup(
 // MAGIC payment_type INT,
+// MAGIC abbreviation STRING,
 // MAGIC description STRING)
 // MAGIC USING parquet
 // MAGIC LOCATION 'wasbs://refdata@gaiasa.blob.core.windows.net/payment-type/';
@@ -154,11 +155,138 @@
 // MAGIC DROP TABLE IF EXISTS refdata_vendor_lookup;
 // MAGIC 
 // MAGIC CREATE TABLE IF NOT EXISTS refdata_vendor_lookup(
-// MAGIC vendorid INT,
+// MAGIC vendor_id INT,
 // MAGIC abbreviation STRING,
 // MAGIC description STRING)
 // MAGIC USING parquet
 // MAGIC LOCATION 'wasbs://refdata@gaiasa.blob.core.windows.net/vendor/';
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC use nyc_db;
+// MAGIC 
+// MAGIC DROP TABLE IF EXISTS trips_yellow_transformed_prq;
+// MAGIC 
+// MAGIC CREATE TABLE IF NOT EXISTS trips_yellow_transformed_prq(
+// MAGIC taxi_type STRING,
+// MAGIC vendor_id STRING,
+// MAGIC pickup_datetime TIMESTAMP,
+// MAGIC dropoff_datetime TIMESTAMP,
+// MAGIC store_and_fwd_flag STRING,
+// MAGIC rate_code_id INT,
+// MAGIC pickup_location_id INT,
+// MAGIC dropoff_location_id INT,
+// MAGIC pickup_longitude STRING,
+// MAGIC pickup_latitude STRING,
+// MAGIC dropoff_longitude STRING,
+// MAGIC dropoff_latitude STRING,
+// MAGIC passenger_count INT,
+// MAGIC trip_distance DOUBLE,
+// MAGIC fare_amount DOUBLE,
+// MAGIC extra DOUBLE,
+// MAGIC mta_tax DOUBLE,
+// MAGIC tip_amount DOUBLE,
+// MAGIC tolls_amount DOUBLE,
+// MAGIC improvement_surcharge DOUBLE,
+// MAGIC total_amount DOUBLE,
+// MAGIC payment_type STRING,
+// MAGIC trip_year STRING,
+// MAGIC trip_month STRING,
+// MAGIC vendor_abbreviation STRING,
+// MAGIC vendor_description STRING,
+// MAGIC month_name_short STRING,
+// MAGIC month_name_full STRING,
+// MAGIC payment_type_description STRING,
+// MAGIC rate_code_description STRING,
+// MAGIC pickup_borough STRING,
+// MAGIC pickup_zone STRING,
+// MAGIC pickup_service_zone STRING,
+// MAGIC dropoff_borough STRING,
+// MAGIC dropoff_zone STRING,
+// MAGIC dropoff_service_zone STRING,
+// MAGIC pickup_year INT,
+// MAGIC pickup_month INT,
+// MAGIC pickup_day INT,
+// MAGIC pickup_hour INT,
+// MAGIC pickup_minute INT,
+// MAGIC pickup_second INT,
+// MAGIC dropoff_year INT,
+// MAGIC dropoff_month INT,
+// MAGIC dropoff_day INT,
+// MAGIC dropoff_hour INT,
+// MAGIC dropoff_minute INT,
+// MAGIC dropoff_second INT)
+// MAGIC USING parquet
+// MAGIC partitioned by (trip_year,trip_month)
+// MAGIC LOCATION 'wasbs://transformed@gaiasa.blob.core.windows.net/yellow-taxi/';
+// MAGIC 
+// MAGIC --
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC use nyc_db;
+// MAGIC 
+// MAGIC DROP TABLE IF EXISTS trips_green_transformed_prq;
+// MAGIC 
+// MAGIC CREATE TABLE IF NOT EXISTS trips_green_transformed_prq(
+// MAGIC taxi_type STRING,
+// MAGIC vendor_id INT,
+// MAGIC pickup_datetime TIMESTAMP,
+// MAGIC dropoff_datetime TIMESTAMP,
+// MAGIC store_and_fwd_flag STRING,
+// MAGIC rate_code_id INT,
+// MAGIC pickup_location_id INT,
+// MAGIC dropoff_location_id INT,
+// MAGIC pickup_longitude STRING,
+// MAGIC pickup_latitude STRING,
+// MAGIC dropoff_longitude STRING,
+// MAGIC dropoff_latitude STRING,
+// MAGIC passenger_count INT,
+// MAGIC trip_distance DOUBLE,
+// MAGIC fare_amount DOUBLE,
+// MAGIC extra DOUBLE,
+// MAGIC mta_tax DOUBLE,
+// MAGIC tip_amount DOUBLE,
+// MAGIC tolls_amount DOUBLE,
+// MAGIC ehail_fee DOUBLE,
+// MAGIC improvement_surcharge DOUBLE,
+// MAGIC total_amount DOUBLE,
+// MAGIC payment_type INT,
+// MAGIC trip_type INT,
+// MAGIC trip_year STRING,
+// MAGIC trip_month STRING,
+// MAGIC vendor_abbreviation STRING,
+// MAGIC vendor_description STRING,
+// MAGIC trip_type_description STRING,
+// MAGIC month_name_short STRING,
+// MAGIC month_name_full STRING,
+// MAGIC payment_type_description STRING,
+// MAGIC rate_code_description STRING,
+// MAGIC pickup_borough STRING,
+// MAGIC pickup_zone STRING,
+// MAGIC pickup_service_zone STRING,
+// MAGIC dropoff_borough STRING,
+// MAGIC dropoff_zone STRING,
+// MAGIC dropoff_service_zone STRING,
+// MAGIC pickup_year INT,
+// MAGIC pickup_month INT,
+// MAGIC pickup_day INT,
+// MAGIC pickup_hour INT,
+// MAGIC pickup_minute INT,
+// MAGIC pickup_second INT,
+// MAGIC dropoff_year INT,
+// MAGIC dropoff_month INT,
+// MAGIC dropoff_day INT,
+// MAGIC dropoff_hour INT,
+// MAGIC dropoff_minute INT,
+// MAGIC dropoff_second INT)
+// MAGIC USING parquet
+// MAGIC partitioned by (trip_year,trip_month)
+// MAGIC LOCATION 'wasbs://transformed@gaiasa.blob.core.windows.net/green-taxi/';
+// MAGIC 
+// MAGIC --
 
 // COMMAND ----------
 
